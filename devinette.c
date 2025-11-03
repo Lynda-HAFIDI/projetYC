06108130c8c8ae53377d0710865c842fbdf41ac5
Lynda Hafidi <233025089+Lynda-HAFIDI@users.noreply.github.com>
2025-11-03 17:23:52 +0100

    ehjdrhj

--- devinette.c
+++ devinette.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello World\n");
+int main()
+{
+    int n = 5;
+    int d;
+
+    printf("Devinette\n");
+    printf("Veuillez taper un numéro :\n");
+
+    scanf("%d", &d); 
+
+    if (d == n)
+        printf("Gagné !\n");
+    else
+    {
+        if( d > n)
+            printf("Perdu ! le nombre que vous avez saisi est plus grand \n");
+        if( d < n)
+            printf("Perdu ! le nombre que vous avez saisi est plus petit \n");
+    }
+
     return 0;
-}
+} 
